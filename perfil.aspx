3c39e9a9b0f5fef2145d044a58e9e9644a7fbfcf
PedroRi99 <94012951+PedroRi99@users.noreply.github.com>
2022-03-14 13:41:46 -0300

    20220314_1

---
+++ perfil.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SitePage.Master" AutoEventWireup="true" CodeBehind="perfil.aspx.cs" Inherits="ProjetoFinal.perfil" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="css/logincad.css" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+     
+    
+  <div class="container" >
+    <a class="links" id="paracadastro"></a>
+    <a class="links" id="paralogin"></a>
+     
+    <div class="content">      
+      <!--FORMULÁRIO DE LOGIN-->
+      <div id="login">
+        <form id="form1" runat="server">
+          <h1>Login</h1> 
+          <p> 
+            <label for="tbEmail">Seu e-mail</label>            
+              <asp:TextBox ID="tbEmail" Text="Digite seu E-mail" runat="server"></asp:TextBox>
+          </p>
+           
+          <p> 
+            <label for="tbSenha">Sua Senha</label>
+            
+              <asp:TextBox ID="tbSenha" runat="server" TextMode="Password"></asp:TextBox>
+          </p>
+           
+          <p> 
+            <input type="checkbox" name="manterlogado" id="manterlogado" value="" /> 
+            <label for="manterlogado">Manter-me logado</label>
+          </p>
+           
+          <p> 
+            <asp:Button ID="btnupdate" runat="server" Text="Atualizar" OnClick="btnUpdate_Click" /> 
+          </p>
+
+</asp:Content>
---
+++ perfil.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjetoFinal
+{
+    public partial class perfil : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            String email = tbEmail.Text;
+            String senha = tbSenha.Text;
+            //
+            //capturar a string de conexão
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+            System.Configuration.ConnectionStringSettings connString;
+            connString = rootWebConfig.ConnectionStrings.ConnectionStrings["ConnectionString"];
+            //cria um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from usuario where email = @email and senha = @senha";
+            cmd.Parameters.AddWithValue("email", email);
+            cmd.Parameters.AddWithValue("senha", senha);
+            con.Open();
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+                // Fez a leitura de todas as linha encontradas no banco
+                registro.Read();
+                //Cria o cookie do Login Com email
+                string loginCookie = registro["email"].ToString();
+                HttpCookie login = new HttpCookie("login", loginCookie);
+                Response.Cookies.Add(login);
+
+                //Cria o cookie do id do usuário
+                string IdUserCookie = registro["Id_user"].ToString();
+                HttpCookie IdUser = new HttpCookie("id_user", IdUserCookie);
+                Response.Cookies.Add(IdUser);
+
+                //string cookie = Request.Cookies["login"];
+
+                //direcionar para a pagina principal
+                Response.Redirect("~/index.aspx");
+            }
+        }
+    }
+}
